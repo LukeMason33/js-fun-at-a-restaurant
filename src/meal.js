db5d11c2229cf5f0c2dbf4079eecc787688ebd5a
Luke Mason <lukjmason@gmail.com>
2020-10-09 14:27:23 -0600

    Complete nameMenuItem, createMenuItem, addIngredients

--- src/meal.js
+++ src/meal.js
@@ -1,15 +1,30 @@
-function nameMenuItem() {
+function nameMenuItem(foodItem) {
+  return `Delicious ${foodItem}`
+};
 
+function createMenuItem(menuItemName, price, type) {
+  menuItem = {
+    name: menuItemName,
+    price: price,
+    type: type
+  }
+  return menuItem;
+};
+
+function addIngredients(ingredient1, ingredients) {
+  if (ingredient1 != ingredients[ingredients.length -1]) {
+    ingredients.push(ingredient1);
+  }
+  else {
+    return ingredients;
+  };
 }
-
 
 module.exports = {
   nameMenuItem,
-  // createMenuItem,
-  // addIngredients,
+  createMenuItem,
+  addIngredients,
   // formatPrice,
   // decreasePrice,
   // createRecipe
 }
-
-
